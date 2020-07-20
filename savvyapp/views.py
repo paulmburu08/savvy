8f34dd1bb50ec0acaf129c525216edc02bb6efb9
paulmburu08 <paulmburu08@gmail.com>
2020-07-20 10:04:01 +0300

    add new_profile view function

--- savvyapp/views.py
+++ savvyapp/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render,redirect
 from django.contrib.auth.decorators import login_required
+from .email import send_welcome_email
 
 # Create your views here.
 def index(request):
@@ -13,3 +14,19 @@ def send_email(request):
     name = current_user.username
     send_welcome_email(name, email)
     return redirect(new_profile)
+
+@login_required(login_url='/accounts/login/')
+def new_profile(request):
+    current_user = request.user
+    if request.method == 'POST':
+        form = ProfileForm(request.POST, request.FILES)
+        if form.is_valid():
+            profile = form.save(commit=False)
+            profile.user = current_user
+            profile.save()
+        return HttpResponseRedirect(reverse('profile',args=[str(request.user.id)]))
+
+    else:
+        form = ProfileForm()
+
+    return render(request, 'new_profile.html',{'form':form})
