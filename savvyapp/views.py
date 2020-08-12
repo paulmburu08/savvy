e75213eab1d72cd6e511ed3f36b8016484e1b9b6
paulmburu08 <paulmburu08@gmail.com>
2020-08-13 00:08:02 +0300

    add edit_profile view function

--- savvyapp/views.py
+++ savvyapp/views.py
@@ -169,6 +169,22 @@ def new_profile(request):
     return render(request, 'new_profile.html',{'form':form})
 
 @login_required(login_url='/accounts/login/')
+def edit_profile(request):
+    current_user = request.user
+    if request.method == 'POST':
+        form = ProfileForm(request.POST, request.FILES)
+        if form.is_valid():
+            profile = form.save(commit=False)
+            profile.user = current_user
+            profile.save()
+        return HttpResponseRedirect(reverse('profile',args=[str(request.user.id)]))
+
+    else:
+        form = ProfileForm()
+
+    return render(request, 'edit_profile.html',{'form':form})
+
+@login_required(login_url='/accounts/login/')
 def profile(request,id):
     profile = UserProfile.objects.get(id = id)
 
