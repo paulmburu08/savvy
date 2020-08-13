f761d7590d2cebe6fa1de379c7002d6dc5aae0eb
paulmburu08 <paulmburu08@gmail.com>
2020-08-13 12:25:23 +0300

    add migrations

--- savvy/settings.py
+++ savvy/settings.py
@@ -103,8 +103,7 @@ else:
    }
 
 db_from_env = dj_database_url.config(conn_max_age=500)
-DATABASES['default'].update(db_from_env)
-
+DATABASES['default']= dj_database_url.config()
 DATABASES['default']['ENGINE'] = 'django.contrib.gis.db.backends.postgis'
 
 # Password validation
